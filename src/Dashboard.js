5588e979215cc0256104d4b16a50396f819b6d35
Angel Paunchev <apaunchev@gmail.com>
2018-12-14 20:19:36 +0200

    fix dashboard

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,5 @@
 import React from "react";
-import DateTime from "./DateTime";
 
-const Dashboard = () => (
-  <div className="Dashboard">
-    <DateTime />
-  </div>
-);
+const Dashboard = ({ children }) => <div className="Dashboard">{children}</div>;
 
 export default Dashboard;
