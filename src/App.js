f2c67df3ab257b5c603ad4147acbc1ba1ce73c98
Angel Paunchev <apaunchev@gmail.com>
2018-12-18 13:09:23 +0200

    add extra DateTime widget

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ export default () => (
   <div className="App">
     <Dashboard theme={lightTheme}>
       <DateTime timezone="Europe/Sofia" title="Sofia" />
+      <DateTime timezone="Europe/Madrid" title="Madrid" />
       <DateTime timezone="America/Costa_Rica" title="San Jose" />
       <GithubIssueCount authKey="github" owner="facebook" repository="react" />
     </Dashboard>
