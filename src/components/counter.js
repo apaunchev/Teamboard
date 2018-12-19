17bae1dbf3ef4e2f319dbaafdf1796037010ece7
Angel Paunchev <apaunchev@gmail.com>
2018-12-19 13:54:04 +0200

    prevent overflow

--- src/components/counter.js
+++ src/components/counter.js
@@ -6,6 +6,8 @@ const Counter = styled.div`
   font-size: ${modularScale(5)};
   font-weight: 500;
   color: ${props => props.theme.palette.accentColor};
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export default ({ value }) => <Counter>{value}</Counter>;
