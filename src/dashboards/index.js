12f54a09c2bd12609219b7dd807a8df67c588e56
Angel Paunchev <apaunchev@gmail.com>
2019-05-19 12:39:40 +0300

    replace light with dark theme

--- src/dashboards/index.js
+++ src/dashboards/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Dashboard from "../components/dashboard";
 import DateTime from "../components/widgets/datetime";
-import lightTheme from "../themes/light-theme";
+import theme from "../themes/dark-theme";
 
 export default () => (
   <div className="App">
-    <Dashboard theme={lightTheme}>
+    <Dashboard theme={theme}>
       <DateTime timezone="Europe/London" title="London" />
     </Dashboard>
   </div>
---
+++ src/themes/dark-theme.js
@@ -0,0 +1,17 @@
+const colors = {
+  black: "#121212",
+  white: "#fff",
+  green: "#45B29D",
+  yellow: "#EFC94C",
+  red: "#DF5A49"
+};
+
+export default {
+  palette: {
+    backgroundColor: colors.black,
+    textColor: colors.white,
+    greenColor: colors.green,
+    yellowColor: colors.yellow,
+    redColor: colors.red
+  }
+};
