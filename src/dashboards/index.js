a72ba5473839cf958d2110e3866f067c4311b3d9
Angel Paunchev <apaunchev@gmail.com>
2019-05-25 19:39:07 +0300

    fix widget name

--- src/dashboards/index.js
+++ src/dashboards/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Dashboard from "../components/dashboard";
-import DateTime from "../components/widgets/datetime";
+import LocalTime from "../components/widgets/local-time";
 import theme from "../themes/dark-theme";
 
 export default () => (
   <div className="App">
     <Dashboard theme={theme}>
-      <DateTime timezone="Europe/London" title="London" />
-      <DateTime timezone="Europe/Paris" title="Paris" />
-      <DateTime timezone="Europe/Moscow" title="Moscow" />
+      <LocalTime timezone="Europe/London" title="London" />
+      <LocalTime timezone="Europe/Paris" title="Paris" />
+      <LocalTime timezone="Europe/Moscow" title="Moscow" />
     </Dashboard>
   </div>
 );
