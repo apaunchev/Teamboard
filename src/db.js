17092b7235432391f1def612056607dc31498cf8
Angel Paunchev <apaunchev@gmail.com>
2019-05-25 12:57:05 +0300

    init lowdb

---
+++ src/db.js
@@ -0,0 +1,7 @@
+import low from "lowdb";
+import LocalStorage from "lowdb/adapters/LocalStorage";
+
+const adapter = new LocalStorage("db");
+export const db = low(adapter);
+
+db.defaults({}).write();
