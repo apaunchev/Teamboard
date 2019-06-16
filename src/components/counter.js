bab985f30ae973c93e9c66c1313009428406a0cd
Angel Paunchev <apaunchev@gmail.com>
2019-06-16 11:24:19 +0300

    show percentage change

--- src/components/counter.js
+++ src/components/counter.js
@@ -12,20 +12,69 @@ const Container = styled.div`
   text-overflow: ellipsis;
 `;
 
-const Counter = ({ title, value, history, showGraph, graphColors }) => (
-  <Container title={title}>
-    {value}
-    {showGraph ? (
-      <Trend
-        data={history}
-        strokeWidth={2}
-        smooth
-        gradient={graphColors}
-        style={{ position: "absolute", right: 0, bottom: 0, left: 0 }}
-      />
-    ) : null}
-  </Container>
-);
+const PercentChange = styled.span`
+  padding-left: 10px;
+  font-size: ${modularScale(-2)};
+  color: ${props => props.color};
+`;
+
+const getPercentChange = data => {
+  if (!data || !data.length) return;
+  const first = data[0].value;
+  const last = data[data.length - 1].value;
+  const periodLength = data.length;
+  return (((last - first) / first / periodLength) * 100).toFixed(2);
+};
+
+const getSymbol = percentChange => {
+  const UPWARDS_ARROW = "▲";
+  const DOWNWARDS_ARROW = "▼";
+  return percentChange > 0 ? UPWARDS_ARROW : DOWNWARDS_ARROW;
+};
+
+const getColor = (percentChange, inverseGraph) => {
+  let color;
+  if (percentChange > 0) {
+    color = inverseGraph ? "#45B29D" : "#DF5A49";
+  } else {
+    color = inverseGraph ? "#DF5A49" : "#45B29D";
+  }
+  return color;
+};
+
+const Counter = ({
+  title,
+  value,
+  history,
+  showGraph,
+  graphColors = ["#558fed"],
+  inverseGraph
+}) => {
+  const percentChange = getPercentChange(history);
+  const symbol = getSymbol(percentChange);
+  const color = getColor(percentChange, inverseGraph);
+
+  return (
+    <Container title={title}>
+      {value}
+      {showGraph ? (
+        <>
+          <PercentChange color={color}>
+            <small style={{ fontSize: "24px" }}>{symbol}</small>
+            {Math.abs(percentChange)}
+            <small style={{ fontSize: "24px" }}>%</small>
+          </PercentChange>
+          <Trend
+            data={history}
+            strokeWidth={2}
+            gradient={graphColors}
+            style={{ position: "absolute", right: 0, bottom: 0, left: 0 }}
+          />
+        </>
+      ) : null}
+    </Container>
+  );
+};
 
 Counter.propTypes = {
   title: PropTypes.string,
@@ -37,7 +86,8 @@ Counter.propTypes = {
     })
   ),
   showGraph: PropTypes.bool,
-  graphColors: PropTypes.arrayOf(PropTypes.string)
+  graphColors: PropTypes.arrayOf(PropTypes.string),
+  inverseGraph: PropTypes.bool
 };
 
 export default Counter;
